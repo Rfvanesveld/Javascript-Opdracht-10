17a0bea77e9e8ddb0775ea00113b21911e52c481
Rfvanesveld <75245865+Rfvanesveld@users.noreply.github.com>
2021-04-26 10:16:06 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,34 @@
+// Function Declaration
+
+// squaredCalculation = function (number1, number2) {
+//     const sum1 = number1 * number1;
+//     const sum2 = number2 * number2;
+//     const total = sum1 + sum2;
+//     const squaredTotal = total * total;
+//     return squaredTotal;
+// }
+// console.log(squaredCalculation(2, 4));
+
+
+// Function Expression
+
+// const squaredCalculation = function (number1, number2) {
+//     const sum1 = number1 * number1;
+//     const sum2 = number2 * number2;
+//     const total = sum1 + sum2;
+//     const squaredTotal = total * total;
+//     return squaredTotal;
+// }
+// console.log(squaredCalculation(2, 4));
+
+
+// Arrow Function (Also function )
+
+const squaredCalculation = (number1, number2) => {
+    const sum1 = number1 * number1;
+    const sum2 = number2 * number2;
+    const total = sum1 + sum2;
+    const squaredTotal = total * total;
+    return squaredTotal;
+}
+console.log(squaredCalculation(2, 4));
